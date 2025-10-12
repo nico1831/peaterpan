ca9cbe44516dddca60a60413123b78776311b929
nico9696 <nicholasbenedictdysy@gmail.com>
2025-10-12 17:11:29 +0900

    copy paste from old code

--- urls.py
+++ urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('store/', include('store.urls')),
+    path('', include('user_management.urls')),
 ]
